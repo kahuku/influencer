daa3e605326253ae44443b8fbe5168ffe381152a
kahuku <66642439+kahuku@users.noreply.github.com>
2022-03-11 00:27:30 -0700

    Add files via upload

---
+++ src/mock-data.js
@@ -0,0 +1,139 @@
+let mock = [
+    {
+        image: '1.jpg',
+        likes: '385365',
+        comments: '5357'
+    },
+    {
+        image: '2.jpg',
+        likes: '700991',
+        comments: '3186'
+    },
+    {
+        image: '3.jpg',
+        likes: '120493',
+        comments: '1936'
+    },
+    {
+        image: '4.jpg',
+        likes: '929767',
+        comments: '14420'
+    },
+    {
+        image: '5.jpg',
+        likes: '1657742',
+        comments: '35262'
+    },
+    {
+        image: '6.jpg',
+        likes: '891034',
+        comments: '8697'
+    },
+    {
+        image: '7.jpg',
+        likes: '260859',
+        comments: '4129'
+    },
+    {
+        image: '8.jpg',
+        likes: '408061',
+        comments: '5090'
+    },
+    {
+        image: '9.jpg',
+        likes: '278149',
+        comments: '4823'
+    },
+    {
+        image: '10.jpg',
+        likes: '154526',
+        comments: '1737'
+    },
+    {
+        image: '11.jpg',
+        likes: '171940',
+        comments: '2613'
+    },
+    {
+        image: '12.jpg',
+        likes: '98051',
+        comments: '996'
+    },
+    {
+        image: '13.jpg',
+        likes: '221002',
+        comments: '3555'
+    },
+    {
+        image: '14.jpg',
+        likes: '255096',
+        comments: '3112'
+    },
+    {
+        image: '15.jpg',
+        likes: '391493',
+        comments: '2273'
+    },
+    {
+        image: '16.jpg',
+        likes: '50558',
+        comments: '922'
+    },
+    {
+        image: '17.jpg',
+        likes: '270743',
+        comments: '1284'
+    },
+    {
+        image: '18.jpg',
+        likes: '271067',
+        comments: '3575'
+    },
+    {
+        image: '19.jpg',
+        likes: '288293',
+        comments: '2009'
+    },
+    {
+        image: '20.jpg',
+        likes: '97803',
+        comments: '881'
+    },
+    {
+        image: '21.jpg',
+        likes: '219955',
+        comments: '4097'
+    },
+    {
+        image: '22.jpg',
+        likes: '383071',
+        comments: '5403'
+    },
+    {
+        image: '23.jpg',
+        likes: '296784',
+        comments: '2163'
+    },
+    {
+        image: '24.jpg',
+        likes: '314152',
+        comments: '3145'
+    },
+    {
+        image: '25.jpg',
+        likes: '272936',
+        comments: '2099'
+    },
+    {
+        image: '26.jpg',
+        likes: '60715',
+        comments: '488'
+    },
+    {
+        image: '27.jpg',
+        likes: '401445',
+        comments: '4766'
+    },
+]
+
+export default mock;
---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import FeedView from '../views/FeedView.vue'
+import AnalyticView from '../views/AnalyticView.vue'
+import IncomeView from '../views/IncomeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/feeds',
+    name: 'feeds',
+    component: FeedView
+  },
+  {
+    path: '/analytics',
+    name: 'analytics',
+    component: AnalyticView
+  },
+  {
+    path: '/income',
+    name: 'income',
+    component: IncomeView
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
